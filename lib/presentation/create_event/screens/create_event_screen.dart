9ce17f216964dae7f2bf4720314950abfd72e475
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2024-12-07 08:15:26 -0500

    refactor: :rocket: refactor create evetn

--- lib/presentation/create_event/screens/create_event_screen.dart
+++ lib/presentation/create_event/screens/create_event_screen.dart
@@ -3,19 +3,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:town_square/presentation/create_event/views/create_event_desktop_view.dart';
 import 'package:town_square/presentation/create_event/views/create_event_mobile_view.dart';
 import 'package:town_square/presentation/shared/providers/device_type_provider.dart';
+import 'package:town_square/presentation/shared/widgets/custom_app_bar_widget.dart';
 
 class CreateEventScreen extends ConsumerWidget {
   const CreateEventScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final textTheme = Theme.of(context).textTheme;
     final size = MediaQuery.sizeOf(context);
     final deviceType = ref.watch(deviceTypeProvider);
 
     return Scaffold(
-      appBar:
-          (deviceType == DeviceType.mobile) ? _buildAppBar(textTheme) : null,
+      appBar: (deviceType == DeviceType.mobile)
+          ? const CustomAppBarWidget(
+              title: 'Create Event',
+            )
+          : null,
       body: SingleChildScrollView(
         child: SizedBox(
           width: size.width * 1,
